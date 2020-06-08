c8ebf818061368d455b0bbd81b95b5de51ea90bd
Mayank Mani <many.mayank@gmail.com>
2020-06-08 19:36:11 +0530

    Updates

---
+++ precache-manifest.b219eb8943e66fab5bd7c261efcb5a08.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b99401d99e911bb87406b5a63a676df",
+    "url": "/Keeper-App/index.html"
+  },
+  {
+    "revision": "cf4fe206b5c0b5437c90",
+    "url": "/Keeper-App/static/js/2.7837ac13.chunk.js"
+  },
+  {
+    "revision": "4499c22e32dbb0ee8a31",
+    "url": "/Keeper-App/static/js/main.c2bc6a24.chunk.js"
+  },
+  {
+    "revision": "17680d16adaada3292ed",
+    "url": "/Keeper-App/static/js/runtime-main.fe0024fd.js"
+  }
+]);
